c9cc0c19b0b7e3b920d2ee3eeca6d92547b22897
Mikko Värri <mikko@varri.fi>
2024-03-10 13:22:14 +0200

    Minimal Java (17) application

---
+++ src/main/java/org/example/hello/cli/Main.java
@@ -0,0 +1,8 @@
+package org.example.hello.cli;
+
+public class Main {
+    public static void main(String... args){
+        System.out.println("Hello, World! Running on Java "
+                               + System.getProperty("java.version"));
+    }
+}
